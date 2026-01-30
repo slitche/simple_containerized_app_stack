2cc2b8315c5f59e2bea962e892116c77292b0ed5
Kulas <randelguevarra@gmail.com>
2026-01-30 16:07:57 +0800

    Uploading workflow file with updated env variables

--- terraform_infra/terra.tf
+++ terraform_infra/terra.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
-  # backend "s3" {
-  #   # this should be retained so that terraform will create state file in s3 bucket
-  #   # even if there's an error in provisioning
+  backend "s3" {
+    # this should be retained so that terraform will create state file in s3 bucket
+    # even if there's an error in provisioning
 
-  #   # backend config variables are defined in GitHub Actions workflow file/CLI
-  # }
+    # backend config variables are defined in GitHub Actions workflow file/CLI
+  }
 
   required_providers {
     aws = {
